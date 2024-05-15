1492382202ac54c0c7a20fee592cf994f4b4be7c
Team4God <T4GMEDIA@MacBook-Pro-de-macbook.local>
2024-05-15 17:10:29 +0530

    creation of shared components

---
+++ client/src/components/shared/navbar.js
@@ -0,0 +1,13 @@
+
+function NavBar() {
+    return(
+        <nav className="navbar">
+            <a className="navbar-link-item">My stats</a>
+            <a className="navbar-link-item">Contents</a>
+            <a className="navbar-link-item">Class</a>
+            <a className="navbar-link-item">Content creator communities</a>
+        </nav>
+    )
+}
+
+export default NavBar;
