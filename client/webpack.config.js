8ec67f8f830fc63bd95f133b3b129cbd569f7c53
Zulan <cameron@visoroy.com>
2024-06-30 22:02:32 +0530

    Updated webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,40 +1,10 @@
-require("dotenv").config();
-const path = require("path");
+require('dotenv').config();
 
 module.exports = {
-  mode: process.env.NODE_ENV || "development",
-
-  entry: "./src/index.js",
-
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-
-
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-    ],
-  },
-
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-
+  mode: process.env.NODE_ENV || 'development',
   devServer: {
     compress: true,
-    host: process.env.HOST || "localhost",
-    static: path.join(__dirname, "public"),
-    port: 8080,
+    host: process.env.HOST || 'localhost',
+    port: process.env.PORT || 8080,
   },
 };
